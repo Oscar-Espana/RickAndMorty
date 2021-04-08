0880b0270dad694c1ca621b30901b29198e732bd
Oscar España <espaaoscar@gmail.com>
2021-04-07 23:32:15 -0500

    Llamada al API de Rick and Morty con useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Characters from "./components/Characters";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Hola Mundo
+      <Characters />
     </div>
   );
 }
